7f8aa0a35600d3d2e47abf4fc8a2e1ece68c179e
Guilherme Mena <sirens.js@gmail.com>
2020-12-06 19:45:22 -0300

    chore: add scripts for jest

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,7 +1,14 @@
 module.exports = {
+  roots: ['<rootDir>/src'],
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules'],
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
   coverageDirectory: 'coverage',
   transform: {
     '.+\\.ts$': 'ts-jest'
