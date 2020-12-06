1ee59850d63e664d080fba72edbdcd9c068a0300
Guilherme Mena <sirens.js@gmail.com>
2020-12-06 18:12:16 -0300

    feat: ensure SignUpController calls addAccount with correct values

--- server/src/presentation/controllers/signup.ts
+++ server/src/presentation/controllers/signup.ts
@@ -5,14 +5,17 @@ import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { PhoneValidator } from '../protocols/phone-validator'
+import { AddAccount } from '../../domain/usecases/add-account'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
   private readonly phoneValidator: PhoneValidator
+  private readonly addAccount: AddAccount
 
-  constructor (emailValidator: EmailValidator, phoneValidator: PhoneValidator) {
+  constructor (emailValidator: EmailValidator, phoneValidator: PhoneValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
     this.phoneValidator = phoneValidator
+    this.addAccount = addAccount
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
@@ -30,7 +33,7 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      const { email, phone, password, passwordConfirmation } = httpRequest.body
+      const { name, surname, email, phone, password, passwordConfirmation } = httpRequest.body
       const isEmailValid = this.emailValidator.isValid(email)
       if (!isEmailValid) {
         return badRequest(new InvalidParamError('email'))
@@ -42,6 +45,13 @@ export class SignUpController implements Controller {
       if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
+      this.addAccount.add({
+        name,
+        surname,
+        email,
+        phone,
+        password
+      })
     } catch {
       return serverError()
     }
