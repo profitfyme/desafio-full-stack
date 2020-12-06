6c8fbf828b9da8ca0df4275d9552cdd7f38241b6
Guilherme Mena <sirens.js@gmail.com>
2020-12-06 19:05:27 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- server/src/presentation/controllers/signup.spec.ts
+++ server/src/presentation/controllers/signup.spec.ts
@@ -347,4 +347,28 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('Should return 200 if valid data is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        surname: 'valid_surname',
+        email: 'valid_email',
+        phone: 'valid_phone',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      surname: 'valid_surname',
+      email: 'valid_email',
+      phone: 'valid_phone',
+      password: 'valid_password'
+    })
+  })
 })
