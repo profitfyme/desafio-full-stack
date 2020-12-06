b6bcf134f4f0444a7bb1020436c64a3f73c86eb9
Guilherme Mena <sirens.js@gmail.com>
2020-12-06 16:30:28 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- server/src/presentation/controllers/signup.ts
+++ server/src/presentation/controllers/signup.ts
@@ -1,6 +1,7 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { InvalidParamError } from '../errors/invalid-param-error'
+import { ServerError } from '../errors/server-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
@@ -16,26 +17,33 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = [
-      'name',
-      'surname',
-      'email',
-      'phone',
-      'password',
-      'passwordConfirmation'
-    ]
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = [
+        'name',
+        'surname',
+        'email',
+        'phone',
+        'password',
+        'passwordConfirmation'
+      ]
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-    const isEmailValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isEmailValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
-    const isPhoneValid = this.phoneValidator.isValid(httpRequest.body.phone)
-    if (!isPhoneValid) {
-      return badRequest(new InvalidParamError('phone'))
+      const isEmailValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isEmailValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+      const isPhoneValid = this.phoneValidator.isValid(httpRequest.body.phone)
+      if (!isPhoneValid) {
+        return badRequest(new InvalidParamError('phone'))
+      }
+    } catch {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
