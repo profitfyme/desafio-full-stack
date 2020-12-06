c97e6607da6cf1f482ec7c057d081d85ea60fce5
Guilherme Mena <sirens.js@gmail.com>
2020-12-06 14:29:46 -0300

    test: ensure SignUpController returns 400 if no phone is provided

--- server/src/presentation/controllers/signup.spec.ts
+++ server/src/presentation/controllers/signup.spec.ts
@@ -49,4 +49,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
+
+  test('Should return 400 if no phone is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        surname: 'any_surname',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('phone'))
+  })
 })
