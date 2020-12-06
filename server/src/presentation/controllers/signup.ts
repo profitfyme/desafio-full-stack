d41594aa19970cf4a49745859aa7985151eb2ef0
Guilherme Mena <sirens.js@gmail.com>
2020-12-06 15:47:10 -0300

    feat: ensure SignUpController returns 400 if an invalid phone is provided

--- server/src/presentation/controllers/signup.ts
+++ server/src/presentation/controllers/signup.ts
@@ -4,12 +4,15 @@ import { InvalidParamError } from '../errors/invalid-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
+import { PhoneValidator } from '../protocols/phone-validator'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly phoneValidator: PhoneValidator
 
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, phoneValidator: PhoneValidator) {
     this.emailValidator = emailValidator
+    this.phoneValidator = phoneValidator
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
@@ -26,9 +29,13 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValid) {
+    const isEmailValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isEmailValid) {
       return badRequest(new InvalidParamError('email'))
+    }
+    const isPhoneValid = this.phoneValidator.isValid(httpRequest.body.phone)
+    if (!isPhoneValid) {
+      return badRequest(new InvalidParamError('phone'))
     }
   }
 }
