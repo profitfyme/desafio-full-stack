3892a12f98f0f96cd4f37fb11b0ec3e7a626ffc1
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-12-08 18:23:30 -0300

    feat(main): add hooks for authentication

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,19 @@
-function App() {
-  return (
-    <h1>Hello world</h1>
-  );
-}
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import GlobalStyle from './styles/global';
+
+import AppProvider from './hooks';
+
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <Router>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
+
+    <GlobalStyle />
+  </Router>
+)
 
 export default App;
