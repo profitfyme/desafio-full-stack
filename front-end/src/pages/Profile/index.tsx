00f451d353d34ebfa9c4046bec721d40ef8569d9
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-12-08 18:24:13 -0300

    feat(main): add pages and routing

---
+++ front-end/src/pages/Profile/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+const Profile: React.FC = () => {
+  return (
+    <Container>
+      <h1>Profile</h1>
+    </Container>
+  )
+}
+
+export default Profile;
---
+++ front-end/src/pages/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <h1>SignIn</h1>
+    </Container>
+  );
+}
+
+export default SignIn;
