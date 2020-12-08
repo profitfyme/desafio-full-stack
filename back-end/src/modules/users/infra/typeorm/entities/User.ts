53cf6d92ea11bb251ca805919082df4449bd2191
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-12-07 21:51:37 -0300

    feat(test): add two unitarios, service create user

---
+++ back-end/src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,39 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { Exclude } from 'class-transformer';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  surname: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  telephone: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
